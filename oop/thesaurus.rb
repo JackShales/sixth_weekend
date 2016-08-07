525d708b73477d89182c62312d53c9f58e21fba5
Jack Shales <jackshalesmusic@gmail.com>
2016-08-07 09:03:34 -0500

    add thesaurus

--- oop/thesaurus.rb
+++ oop/thesaurus.rb
@@ -12,3 +12,95 @@
 # and which belongs in the Entry class.
 #
 # And... test your functionality using RSpec!
+
+class Entry
+  attr_reader :word, :synonyms, :antonyms
+
+  def initialize(word)
+    @word = word
+    @synonyms = []
+    @antonyms = []
+  end
+
+  def add_synonym!(synonym)
+    @synonyms << synonym
+  end
+
+  def add_antonym!(antonym)
+    @antonyms << antonym
+  end
+end
+
+class Thesaurus
+  attr_reader :entries
+
+  def initialize
+    @entries = []
+  end
+
+  def new_word(word)
+    x = Entry.new(word)
+    @entries << x
+  end
+
+  def delete_word(word)
+    @entries.delete_if { |entry| entry.word == word }
+  end
+end
+
+RSpec.describe Entry do
+  describe '#initialize' do
+    it 'should contain a word' do
+      entry = Entry.new("sly")
+      expect(entry.word).to eq("sly")
+    end
+
+    it 'should contain an empty array of synonyms' do
+      entry = Entry.new("sly")
+      expect(entry.synonyms).to eq([])
+    end
+
+    it 'should contain an empty array of antonyms' do
+      entry = Entry.new("sly")
+      expect(entry.antonyms).to eq([])
+    end
+  end
+
+  describe '#add_synonym' do
+    it 'should be able to add synonyms' do
+      entry = Entry.new("sly")
+      entry.add_synonym!("sneaky")
+      expect(entry.synonyms).to eq(["sneaky"])
+    end
+  end
+
+  describe '#add_antonym' do
+    it 'should be able to add antonyms' do
+      entry = Entry.new("sly")
+      entry.add_antonym!("brash")
+      expect(entry.antonyms).to eq(["brash"])
+    end
+  end
+end
+
+RSpec.describe Thesaurus do
+  describe '#new_word' do
+    it 'should start with an empty entries array' do
+      th_1 = Thesaurus.new
+      expect(th_1.entries).to eq([])
+    end
+
+    it 'should shovel a word into entries' do
+      th_1 = Thesaurus.new
+      th_1.new_word("sly")
+      expect(th_1.entries[0].word).to eq("sly")
+    end
+
+    it 'should delete a word from the entries array' do
+      th_1 = Thesaurus.new
+      th_1.new_word("sly")
+      th_1.delete_word("sly")
+      expect(th_1.entries).to eq([])
+    end
+  end
+end
